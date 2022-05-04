53b1c5c8e2f220c92d571d2f7bf6a8cc4c6eb2c2
Janaina Tavares <tjanaina92@gmail.com>
2022-05-03 22:53:13 -0300

    Projeto blog pessoal

---
+++ src/main/java/com/generation/blogpessoal/BlogPessoalJanainaApplication.java
@@ -0,0 +1,13 @@
+package com.generation.blogpessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPessoalJanainaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPessoalJanainaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -0,0 +1,14 @@
+package com.generation.blogpessoal.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.generation.blogpessoal.model.Postagem;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+	public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo);
+	// containing é igual o LIKE do MySQL e IgnoreCase desconsidera diferenças entre M e m
+}
